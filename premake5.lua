70c63746beb7420b0acb252a674a85d15a1e8bc6
SkaLe <81234210+SkaLe3@users.noreply.github.com>
2024-02-29 20:53:05 +0200

    fixes (#1)
    * Setup branch
    
    * generate script
    
    * Added premake
    
    * report
    
    * tests
    
    * project conf fix
    
    * gitignore fix

---
+++ premake5.lua
@@ -0,0 +1,54 @@
+workspace "Compiler"
+	architecture "x64"
+	startproject "Compiler"
+
+	configurations
+	{
+		"Debug",
+		"Release"
+	}
+
+outputdir = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
+
+-- Include directories relative to root folder (solution directory)
+IncludeDir = {}
+
+group ""
+
+
+project "Compiler"
+	location "Compiler"
+	kind "ConsoleApp"
+	language "C++"
+	cppdialect "C++20"
+	staticruntime "off"
+
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+	targetname("ssc")
+
+	files
+	{
+		"%{prj.name}/src/**.h",
+		"%{prj.name}/src/**.cpp"
+	}
+
+	includedirs
+	{
+		"Compiler/src",
+        "Compiler/src/Core"
+	}
+
+	filter "system:windows"
+		systemversion "latest"
+
+	filter "configurations:Debug"
+		defines "COMPILE_DEBUG"
+		runtime "Debug"
+		symbols "on"
+
+	filter "configurations:Release"
+		defines "COMPILE_RELEASE"
+		runtime "Release"
+		optimize "on"
